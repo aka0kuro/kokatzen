a846b8d63e5980efd646ec343709dd5f651ed71e
aka0kuro <50419659+aka0kuro@users.noreply.github.com>
2025-02-22 00:24:08 +0100

    Update kokatzen.sh

--- kokatzen.sh
+++ kokatzen.sh
@@ -2,10 +2,14 @@
 
 # Cleaning the TTY.
 clear
+
+# Color to scipt
 RED="\e[31m"
 GREEN="\e[32m"
+BLUE="\e[94m" 
 ENDCOLOR="\e[0m"
 
+# Logo to script
 logo (){ 
 echo -e "$RED
 ██╗  ██╗ ██████╗ ██╗  ██╗ █████╗ ████████╗███████╗███████╗███╗   ██╗
@@ -17,35 +21,65 @@ echo -e "$RED
 $ENDCOLOR"
 }
 
+infomation () {
+clear
 logo
+password () {
+	echo
+	# Setting password.
+	if [[ -n $username ]]; then
+		read -s -p "$(echo -e $GREEN"Enter your password: " $ENDCOLOR)" password
+		echo -e "\n"
+		
+		read -s -p "$(echo -e $GREEN"Confir your password: " $ENDCOLOR)" password_confirm
+		echo -e "\n"
+	fi
+	if [[ "$password" == "$password_confirm" ]]; then
+	    echo -e "${GREEN}Passwords match.$ENDCOLOR"
+	else
+	    echo -e "${RED}Passwords do not match.$ENDCOLOR" && sleep 3 && clear && logo && password
+	fi
+}
+echo -e "${GREEN}Selecting the installation method. $ENDCOLOR"
+# Setting username.
+read -r -p "$(echo -e $GREEN"Please enter name for a user account ${RED}(leave empty to skip)${GREEN}: "$ENDCOLOR)" username
+password
+
 echo
+# Choose locales.
+read -r -p "$(echo -e $GREEN"Please insert the locale you use in this format ${RED}(xx_XX)${GREEN}: "$ENDCOLOR)" locale
 
-# Selecting the target for the installation.
-read -r -p "$(echo -e $GREEN"Select the disk where Arch Linux is going to be installed: " $ENDCOLOR)" ENTRY
-select ENTRY in $(lsblk -dpnoNAME|grep -P "/dev/sd|nvme|vd|mm");
-do
-    DISK=$ENTRY
-    echo -e "$GREEN Installing Arch Linux on $DISK.$ENDCOLOR"
-    break
-done
+echo
+# Choose keyboard layout.
+read -r -p "$(echo -e $GREEN"Please insert the keyboard layout you use: ${RED}(es)${GREEN}: "$ENDCOLOR)" kblayout
 
-# Confirming the disk selection.
-read -r -p "$(echo -e $GREEN"This will delete the current partition table on $DISK. Do you agree [y/N]?" $ENDCOLOR)" response
-response=${response,,}
-if [[ ! ("$response" =~ ^(yes|y)$) ]]; then
-    echo -e "$RED Quitting. $ENDCOLOR"
-    exit
-fi
+echo
+# Choose timezone.
+read -r -p "$(echo -e $GREEN"Please insert the timezone you use: ${RED}(Europe/Madrid)${GREEN}: "$ENDCOLOR)" timezone
+
+echo
+# Choose languaje.
+read -r -p "$(echo -e $GREEN"Please insert the language you use: ${RED}(en_US.UTF-8)${GREEN}: "$ENDCOLOR)" language
+
+echo
+read -p "$(echo -e $GREEN"Press Enter to continue, otherwise press any other key. "$ENDCOLOR)" start_install
+
+}
 
 # Selecting the kernel flavor to install.
 kernel_selector () {
-    echo "List of kernels:"
-    echo "1) Stable — Vanilla Linux kernel and modules, with a few patches applied."
-    echo "2) Hardened — A security-focused Linux kernel."
-    echo "3) Longterm — Long-term support (LTS) Linux kernel and modules."
-    echo "4) Zen Kernel — Optimized for desktop usage."
+	clear
+	logo
+
+	echo -e "${BLUE}Selecting the kernel to install. $ENDCOLOR"
+	echo
+    echo -e "${GREEN}List of kernels: $ENDCOLOR"
+    echo -e "${RED}1)$GREEN Stable — Vanilla Linux kernel and modules, with a few patches applied. $ENDCOLOR"
+    echo -e "${RED}2)$GREEN Hardened — A security-focused Linux kernel. $ENDCOLOR"
+    echo -e "${RED}3)$GREEN Longterm — Long-term support (LTS) Linux kernel and modules. $ENDCOLOR"
+    echo -e "${RED}4)$GREEN Zen Kernel — Optimized for desktop usage. $ENDCOLOR"
     read -r -p "Insert the number of the corresponding kernel: " choice
-    echo "$choice will be installed"
+
     case $choice in
         1 ) kernel=linux
             ;;
@@ -55,68 +89,88 @@ kernel_selector () {
             ;;
         4 ) kernel=linux-zen
             ;;
-        * ) echo "You did not enter a valid selection."
+        * ) echo -e "${REED}You did not enter a valid selection. $ENDCOLOR" && clear && logo
             kernel_selector
     esac
+	echo -e "${GREEN}The selected kernel is:$BLUE $kernel $ENDCOLOR"
+    sleep 2
+	# Confirming the disk selection.
+	while true; do
+		echo
+		read -r -p "$(echo -e $GREEN"Correct kernel selection?. Do you agree $RED[y/N]$GREEN?" $ENDCOLOR)" response
+		response=${response,,}
+		if [[ "$response" =~ ^(yes|y)$ ]]; then
+			infomation
+			break 
+		elif [[ ! "$response" =~ ^(yes|y)$ ]]; then
+			echo
+			echo -e "${RED}Invalid response. Please type 'yes' or 'y' to continue.${ENDCOLOR}" & sleep 2 & kernel_selector
+		fi
+	done
 }
-clear
-logo
 
-echo "Selecting the kernel to install."
-kernel_selector
+disk () {
+	clear
+	logo
+	echo
 
-clear
-logo
-echo "Selecting the installation method."
-# Setting username.
-read -r -p "Please enter name for a user account (leave empty to skip): " username
+	# Selecting the target for the installation.
+	disks=$(lsblk -dpno NAME | grep -P "/dev/sd|nvme|vd|mm")
 
-echo
-# Setting password.
-if [[ -n $username ]]; then
-    read -r -p -s "Please enter a password for the user account: " password
-fi
+	counter=1
+	declare -a disk_array
+	for disk in $disks; do
+		echo -e "${BLUE}[$counter] $disk${ENDCOLOR}"
+		disk_array+=("$disk")
+		((counter++))
+	done
 
-echo
-# Choose locales.
-read -r -p "Please insert the locale you use in this format (xx_XX): " locale
+	while true; do
+		echo -e "${GREEN}Please select a disk (1-${counter}):${ENDCOLOR}"
+		read -r selection
 
-echo
-# Choose keyboard layout.
-read -r -p "Please insert the keyboard layout you use: " kblayout
+		# Validar selección
+		if [[ "$selection" =~ ^[0-9]+$ ]] && [ "$selection" -ge 1 ] && [ "$selection" -lt "$counter" ]; then
+			DISK=${disk_array[$selection-1]}
+			echo
+			echo -e "${GREEN}Installing Arch Linux on$BLUE $DISK.${ENDCOLOR}"
+			break
+		else     
+			echo
+			echo -e "${RED}Invalid selection. Please try again.${ENDCOLOR}" && sleep 2 && disk
+		fi
+	done
 
-echo
-# Choose timezone.
-read -r -p "Please insert the timezone you use: (Europe/Madrid)" timezone
+# Confirming the disk selection.
+while true; do
+	echo
+	read -r -p "$(echo -e $GREEN"This will delete the current partition table on $DISK. Do you agree $RED[y/N]$GREEN?" $ENDCOLOR)" response
+	response=${response,,}
+	if [[ "$response" =~ ^(yes|y)$ ]]; then
+		kernel_selector
+		break 
+	elif [[ ! "$response" =~ ^(yes|y)$ ]]; then
+		echo -e "${RED}Invalid response. Please type 'yes' or 'y' to continue.${ENDCOLOR}" & disk
+	fi
+done
+}
+disk
 
-echo
-# Choose languaje.
-read -r -p "Please insert the language you use: (en_US.UTF-8)" language
-
-BASE_PKGS="base sudo linux-firmware iptables-nft python nano git python linux-$kernel_selector-headers networkmanager dosfstools e2fsprogs btrfs-progs man-db"
-
-echo
-read -p "Press Enter to continue, otherwise press any other key. " start_install
 
 if [[ -n $start_install ]] ; then
     exit 1
 fi
 
+BASE_PKGS="base sudo linux-firmware iptables-nft python nano git python linux-$kernel_selector-headers networkmanager dosfstools e2fsprogs btrfs-progs man-db"
 
-echo "
-######################################################
-# Verify the boot mode
-# https://wiki.archlinux.org/title/Installation_guide#Verify_the_boot_mode
-######################################################
-"
 if [[ -e /sys/firmware/efi/efivars ]] ; then
-    echo "UEFI mode OK."
+    echo -e "${GREEN}UEFI mode OK.${ENDCOLOR}"
 else
-    echo "System not booted in UEFI mode!"
+    echo -e "${RED}System not booted in UEFI mode!${ENDCOLOR}"
     exit 1
 fi
 echo -e "\n"
-read -p "Do you want to set up secure boot with your own key? [Y/n] " secure_boot
+read -p "$(echo -e $GREEN"Do you want to set up secure boot with your own key? $RED[Y/n] " $ENDCOLOR)" secure_boot
 secure_boot="${secure_boot:-y}"
 secure_boot="${secure_boot,,}"
 
@@ -126,8 +180,8 @@ if [[ $secure_boot == y ]] ; then
     # Secure Boot: disabled (setup)
     setup_mode=$(bootctl status | grep -E "Secure Boot.*setup" | wc -l)
     if [[ $setup_mode -ne 1 ]] ; then
-        echo "The firmware is not in the setup mode. Please check BIOS."
-        read -p "Continue without secure boot? [y/N] " keep_going
+        echo -e "${GREEN}The firmware is not in the setup mode. Please check BIOS.$ENDCOLOR"
+        read -p "$(echo -e $GREEN"Continue without secure boot? ${RED}[y/N] " $ENDCOLOR)" keep_going
         keep_going="${keep_going:-n}"
         keep_going="${keep_going,,}"
         if [[ keep_going == y ]] ; then
@@ -138,22 +192,16 @@ if [[ $secure_boot == y ]] ; then
     fi
 fi
 
-echo "
-######################################################
-# Check internet connection
-# https://wiki.archlinux.org/title/Installation_guide#Connect_to_the_internet
-######################################################
-"
 ping -c 1 archlinux.org > /dev/null
 if [[ $? -ne 0 ]] ; then
     # Function to prompt the user to retry or exit
     retry_or_exit() {
-        echo "Do you want to try again? (y/n)"
+        echo -e "${GREEN}Do you want to try again? ${RED}(y/n)${ENDCOLOR}"
         read -r choice
         case "$choice" in
             y|Y) return 0 ;;
-            n|N) echo "Exiting..."; exit 1 ;;
-            *) echo "Invalid choice. Exiting..."; exit 1 ;;
+            n|N) echo "${RED}Exiting...${ENDCOLOR}"; exit 1 ;;
+            *) echo -e "${GREEN}Invalid choice. Exiting...${ENDCOLOR}"; exit 1 ;;
         esac
     }
     
@@ -162,31 +210,31 @@ if [[ $? -ne 0 ]] ; then
     
     # Check if any Wi-Fi interfaces were found
     if [ ${#interfaces[@]} -eq 0 ]; then
-        echo "No Wi-Fi interfaces detected. Is your Wi-Fi card enabled?"
+        echo -e "${RED}No Wi-Fi interfaces detected. Is your Wi-Fi card enabled?${ENDCOLOR}"
         exit 1
     fi
     
     # Display interfaces and allow the user to select one
     while true; do
-        echo "Detected Wi-Fi interfaces:"
+        echo -e "${GREEN}Detected Wi-Fi interfaces:${ENDCOLOR}"
         for i in "${!interfaces[@]}"; do
-            echo "[$i] ${interfaces[$i]}"
+            echo -e "${RED}[$i] ${interfaces[$i]}${ENDCOLOR}"
         done
     
-        read -p "Select the number of the Wi-Fi interface: " index
+        read -p "$(echo -e $GREEN"Select the number of the Wi-Fi interface: " $ENDCOLOR)" index
     
         if [[ "$index" =~ ^[0-9]+$ ]] && [ "$index" -lt "${#interfaces[@]}" ]; then
             interface="${interfaces[$index]}"
-            echo "Using interface: $interface"
+            echo -e "${GREEN}Using interface: $interface${ENDCOLOR}"
             break
         else
-            echo "Invalid selection."
+            echo -e "${RED}Invalid selection.${ENDCOLOR}"
             retry_or_exit
         fi
     done
     
     # Scan for available networks
-    echo "Scanning for Wi-Fi networks..."
+    echo -e "${GREEN}Scanning for Wi-Fi networks...${ENDCOLOR}"
     iwctl station "$interface" scan
     sleep 2
     
@@ -195,37 +243,37 @@ if [[ $? -ne 0 ]] ; then
     
     # Check if any networks were found
     if [ ${#networks[@]} -eq 0 ]; then
-        echo "No Wi-Fi networks found. Try again."
+        echo -e "${RED}No Wi-Fi networks found. Try again.${ENDCOLOR}"
         retry_or_exit
     fi
     
     # Display available networks and allow the user to select one
     while true; do
-        echo "Detected Wi-Fi networks:"
+        echo -e "${GREEN}Detected Wi-Fi networks:${ENDCOLOR}"
         for i in "${!networks[@]}"; do
-            echo "[$i] ${networks[$i]}"
+            echo -e "${GREEN}[$i] ${networks[$i]}${ENDCOLOR}"
         done
     
-        read -p "Select the number of the Wi-Fi network to connect to: " net_index
+        read -p "$(echo -e $GREEN"Select the number of the Wi-Fi network to connect to: " $ENDCOLOR)" net_index
     
         if [[ "$net_index" =~ ^[0-9]+$ ]] && [ "$net_index" -lt "${#networks[@]}" ]; then
             network="${networks[$net_index]}"
-            echo "Connecting to: $network"
+            echo -e "${GREEN}Connecting to: ${RED}$network${ENDCOLOR}"
             break
         else
-            echo "Invalid selection."
+            echo "${RED}Invalid selection.${ENDCOLOR}"
             retry_or_exit
         fi
     done
     
     # Request Wi-Fi password
-    read -s -p "Enter the password for $network: " password
+    read -s -p "$(echo -e $GREEN"Enter the password for $network: " $ENDCOLOR)" password
     echo
     
     # Connect to the Wi-Fi network
     iwctl station "$interface" connect "$network" --passphrase "$password"
     
-    echo "Attempting to connect to $network..."
+    echo -e "${GREEN}Attempting to connect to $network...${ENDCOLOR}"
     sleep 3
     
     # Check if the connection was successful
@@ -247,37 +295,18 @@ else
     microcode=intel-ucode
 fi
 
-
-echo "
-######################################################
-# Update the system clock
-# https://wiki.archlinux.org/title/Installation_guide#Update_the_system_clock
-######################################################
-"
 timedatectl set-ntp true
 
-echo "
-######################################################
-# EFI boot settings
-# https://man.archlinux.org/man/efibootmgr.8
-######################################################
-"
 efibootmgr --unicode
 efi_boot_id=" "
 while [[ -n $efi_boot_id ]]; do
     echo -e "\nDo you want to delete any boot entries?: "
-    read -p "Enter boot number (empty to skip): " efi_boot_id
+    read -p "$(echo -e $GREEN"Enter boot number (empty to skip): " $ENDCOLOR)" efi_boot_id
     if [[ -n $efi_boot_id ]] ; then
         efibootmgr --bootnum $efi_boot_id --delete-bootnum --unicode
     fi
 done
 
-echo "
-######################################################
-# Partition disks
-# https://wiki.archlinux.org/title/Installation_guide#Partition_the_disks
-######################################################
-"
 umount -R /mnt
 devices=$(lsblk --nodeps --paths --list --noheadings --sort=size --output=name,size,model | grep --invert-match "loop" | cat --number)
 
@@ -285,7 +314,7 @@ device_id=" "
 while [[ -n $device_id ]]; do
     echo -e "Choose device to format:"
     echo "$devices"
-    read -p "Enter a number (empty to skip): " device_id
+    read -p "$(echo -e $GREEN"Enter a number (empty to skip): " $ENDCOLOR)" device_id
     if [[ -n $device_id ]] ; then
         device=$(echo "$devices" | awk "\$1 == $device_id { print \$2}")
         fdisk "$device"
@@ -297,13 +326,13 @@ partitions=$(lsblk --paths --list --noheadings --output=name,size,model | grep -
 # EFI partition
 echo -e "\n\nTell me the EFI partition number:"
 echo "$partitions"
-read -p "Enter a number: " efi_id
+read -p "$(echo -e $GREEN"Enter a number: " $ENDCOLOR)" efi_id
 efi_part=$(echo "$partitions" | awk "\$1 == $efi_id { print \$2}")
 
 # root partition
 echo -e "\n\nTell me the root partition number:"
 echo "$partitions"
-read -p "Enter a number: " root_id
+read -p "$(echo -e $GREEN"Enter a number: " $ENDCOLOR)" root_id
 root_part=$(echo "$partitions" | awk "\$1 == $root_id { print \$2}")
 
 # Wipe existing LUKS header
@@ -319,7 +348,7 @@ wipefs --all $root_part 2> /dev/null
 # swap is important, see [In defence of swap](https://chrisdown.name/2018/01/02/in-defence-of-swap.html)
 echo -e "\n\nTell me the swap partition number:"
 echo "$partitions"
-read -p "Enter a number or press ENTER to skip: " swap_id
+read -p "$(echo -e $GREEN"Enter a number or press ENTER to skip: " $ENDCOLOR)" swap_id
 if [[ -n $swap_id ]] ; then
     swap_part=$(echo "$partitions" | awk "\$1 == $swap_id { print \$2}") || swap_part=""
 
@@ -328,4 +357,3 @@ if [[ -n $swap_id ]] ; then
     cryptsetup luksDump $swap_part 2> /dev/null
     wipefs --all $swap_part 2> /dev/null
 fi
-
